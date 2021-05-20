43c2e102bb61ed792a51bab5ae6beca6a6f4caa1
root <root@perf19.perf.lab.eng.bos.redhat.com>
2021-05-20 08:50:41 -0400

    changed sysbench depricated argument

--- testmem.sh
+++ testmem.sh
@@ -8,14 +8,14 @@ echo "NPS = $nps" | tee -a $outfile
 echo "$( uname -r )" | tee -a $outfile
 echo "$( cat /etc/redhat-release )" | tee -a $outfile
 echo "==============================================================================================================================" | tee -a $outfile
-echo "sysbench --test=memory --memory-block-size=1K --memory-scope=global --memory-total-size=100G --memory-oper=read run" | tee -a $outfile
-sysbench --test=memory --memory-block-size=1K --memory-scope=global --memory-total-size=100G --memory-oper=read run | tee -a $outfile
+echo "sysbench memory --memory-block-size=1K --memory-scope=global --memory-total-size=100G --memory-oper=read run" | tee -a $outfile
+sysbench memory --memory-block-size=1K --memory-scope=global --memory-total-size=100G --memory-oper=read run | tee -a $outfile
 echo "==============================================================================================================================" | tee -a $outfile
-echo "sysbench --test=memory --memory-block-size=1K --memory-scope=global --memory-total-size=100G --memory-oper=write run" | tee -a $outfile
-sysbench --test=memory --memory-block-size=1K --memory-scope=global --memory-total-size=100G --memory-oper=read run | tee -a $outfile
+echo "sysbench memory --memory-block-size=1K --memory-scope=global --memory-total-size=100G --memory-oper=write run" | tee -a $outfile
+sysbench memory --memory-block-size=1K --memory-scope=global --memory-total-size=100G --memory-oper=read run | tee -a $outfile
 echo "==============================================================================================================================" | tee -a $outfile
-echo "sysbench --test=memory --memory-block-size=1M --memory-total-size=100G run" | tee -a $outfile
-sysbench --test=memory --memory-block-size=1M --memory-total-size=100G run | tee -a $outfile
+echo "sysbench memory --memory-block-size=1M --memory-total-size=100G run" | tee -a $outfile
+sysbench memory --memory-block-size=1M --memory-total-size=100G run | tee -a $outfile
 echo "==============================================================================================================================" | tee -a $outfile
 echo "dmidecode -t memory" | tee -a $outfile
 dmidecode -t memory | tee -a $outfile
